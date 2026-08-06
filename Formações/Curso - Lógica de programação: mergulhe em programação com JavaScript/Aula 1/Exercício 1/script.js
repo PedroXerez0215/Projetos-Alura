d7c55333605f9199732611f9476adee6768561cf
PedroXerez0215 <pedroxerezdev@gmail.com>
2026-08-06 05:28:08 -0300

    Delete Formações/Curso - Lógica de programação: mergulhe em programação com JavaScript directory

--- Formações/Curso - Lógica de programação: mergulhe em programação com JavaScript/Aula 1/Exercício 1/script.js
+++
@@ -1,5 +0,0 @@
-//Exercício: Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
-
-// Código:
-
-alert('Boas vindas ao nosso site!')
--- Formações/Curso - Lógica de programação: mergulhe em programação com JavaScript/Aula 1/Exercício 2/script.js
+++
@@ -1,3 +0,0 @@
-// Declare uma variável chamada "nome" e atribua a ela o valor "Lua"
-
-let nome = "Lua"
